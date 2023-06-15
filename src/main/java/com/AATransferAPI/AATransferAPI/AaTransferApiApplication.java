f7b7096ff8d5afa34020a76c9059ef13535cc681
Robert Caruana <rob.caruana15@gmail.com>
2023-06-15 17:23:30 +0200

    initial API foundation

---
+++ src/main/java/com/AATransferAPI/AATransferAPI/AaTransferApiApplication.java
@@ -0,0 +1,13 @@
+package com.AATransferAPI.AATransferAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaTransferApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaTransferApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AATransferAPI/AATransferAPI/AaTransferApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AATransferAPI.AATransferAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaTransferApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
