606776514923fa78fc3a711a73e8ac95d87a9b0a
Robert Caruana <rob.caruana15@gmail.com>
2023-06-18 10:15:23 +0200

    testing HATEOAS

--- src/main/java/com/AATransferAPI/AATransferAPI/API/AccountController.java
+++ src/main/java/com/AATransferAPI/AATransferAPI/API/AccountController.java
@@ -8,10 +8,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
-@RequestMapping(path="api/v1/account")
+@RequestMapping(path="api/v1/accounts")
 public class AccountController {
 
     private final AccountService accountService;
@@ -22,40 +25,97 @@ public class AccountController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Account>> getAllAccounts(){
-        List<Account> account = accountService.getAllAccounts();
-
-        if(account.isEmpty())
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-
-        return new ResponseEntity<>(account, HttpStatus.OK);
+    public ResponseEntity<?> getAllAccounts(){
+        Map<String, Object> map = new LinkedHashMap<String, Object>();
+        List<Account> accountList = accountService.getAllAccounts();
+        if (!accountList.isEmpty()) {
+            map.put("status", 1);
+            map.put("data", accountList);
+            return new ResponseEntity<>(map, HttpStatus.OK);
+        } else {
+            map.clear();
+            map.put("status", 0);
+            map.put("message", "Data is not found");
+            return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+        }
     }
 
-    @GetMapping(path = "{userID}")
-    public ResponseEntity<List<Account>> getAllAccountsForUser(@PathVariable("userID")Long userID){
+    @GetMapping(path = "/user/{userID}")
+    public ResponseEntity<?> getAllAccountsForUser(@PathVariable("userID")Long userID){
 
-        List<Account> account = accountService.getAllAccountsForUser(userID);
-
-        if(account.isEmpty())
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-
-        return new ResponseEntity<>(account, HttpStatus.OK);
+        Map<String, Object> map = new LinkedHashMap<String, Object>();
+        List<Account> accountList = accountService.getAllAccountsForUser(userID);
+        if (!accountList.isEmpty()) {
+            map.put("status", 1);
+            map.put("data", accountList);
+            return new ResponseEntity<>(map, HttpStatus.OK);
+        } else {
+            map.clear();
+            map.put("status", 0);
+            map.put("message", "Data is not found");
+            return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+        }
     }
 
+    @GetMapping(path = "/account/{accountID}")
+    public ResponseEntity<?> getAccount(@PathVariable("accountID")Long accountID){
+
+        Map<String, Object> map = new LinkedHashMap<String, Object>();
+        Optional<Account> accountO = accountService.getAccount(accountID);
+        if (!accountO.isEmpty()) {
+            map.put("status", 1);
+            map.put("data", accountO);
+            return new ResponseEntity<>(map, HttpStatus.OK);
+        } else {
+            map.clear();
+            map.put("status", 0);
+            map.put("message", "Data is not found");
+            return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+        }
+    }
 
     // When the target argument fails to pass the validation, Spring Boot throws a MethodArgumentNotValidException exception.
     // @RequestBody = convert JSON payload to java object
     // @Valid = checks the annotations in the object itself
     @PostMapping
-    public ResponseEntity<String> insertNewAccountForUser(@Valid @RequestBody Account account){
+    public ResponseEntity<?> insertNewAccountForUser(@Valid @RequestBody Account account){
 
-        if(accountService.insertNewAccountForUser(account).get_accountID() != null)
-            return new ResponseEntity<>("New account created.", HttpStatus.OK);
-
-        return new ResponseEntity<>("Failed to create new account.", HttpStatus.NOT_IMPLEMENTED);
+        Map<String, Object> map = new LinkedHashMap<String, Object>();
+        if(accountService.insertNewAccountForUser(account).get_accountID() != null) {
+            map.put("status", 1);
+            map.put("message", "Record is Saved Successfully!");
+            return new ResponseEntity<>(map, HttpStatus.CREATED);
+        }else {
+            map.clear();
+            map.put("status", 0);
+            map.put("message", "Failed to create new account.");
+            return new ResponseEntity<>(map, HttpStatus.NOT_IMPLEMENTED);
+        }
+    }
+/*
+    @PutMapping("/update/{accountID}{status}")
+    public ResponseEntity<?> updateUserById(@PathVariable Long accountID, @Valid @RequestBody Account account) {
+        Map<String, Object> map = new LinkedHashMap<String, Object>();
+        try {
+            Account oldAccount = accountService.findByAccountId(accountID);
+            user.setUserName(userDetail.getUserName());
+            user.setMobileNo(userDetail.getMobileNo());
+            user.setEmailId(userDetail.getEmailId());
+            user.setCity(userDetail.getCity());
+            user.setPassword(userDetail.getPassword());
+            userService.save(user);
+            map.put("status", 1);
+            map.put("data", userService.findById(id));
+            return new ResponseEntity<>(map, HttpStatus.OK);
+        } catch (Exception ex) {
+            map.clear();
+            map.put("status", 0);
+            map.put("message", "Data is not found");
+            return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+        }
     }
 
-    /*
+
     @PutMapping(path = "{userID}")
     public ResponseEntity<String> updateAccountDetailsOfUser(@PathVariable("userID") UUID userID, @Valid @RequestBody Account account){
 
